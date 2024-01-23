9518bedd7b8a5a7f8676ee16c6afa337d3685c90
Vikram <142532704+Knvikram05@users.noreply.github.com>
2024-01-23 15:07:31 +0530

    Update Calculator.py

--- InternshipTask/CODSOFT/TASK2/Calculator.py
+++ InternshipTask/CODSOFT/TASK2/Calculator.py
@@ -1,37 +1,35 @@
-def add(x,y):
+def add(x, y):
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     return x - y
 
-def multiply(x,y):
+def multiply(x, y):
     return x * y
 
+def divide(x, y):
+    return x / y if y != 0 else "Cannot divide by zero"
 
-def divide(x,y):
-    return x / y if y != 0 else "cannot divide by zero"
+def calculator():
+    operations = {'+': add, '-': subtract, '*': multiply, '/': divide}
+    
+    print("Simple Calculator")
 
-def Calculator():
-    operations = {'+':add , '-': subtract , '*':multiply , '/':divide}
-    print ("Simple Calculator")
-
-    #using  error handling method 
-
+    # Using error handling for user input
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
-    
     except ValueError:
-        print("Invalid input .please Enter valid number")   
+        print("Invalid input. Please enter valid numbers.")
         return
-    
-    operation = input("choose operation ( + , _ , * , / ,):")
+
+    operation = input("Choose operation (+, -, *, /): ")
 
     if operation in operations:
-        result = operations[operation](num1 ,num2)
-        print(f"Result:{result}")
+        result = operations[operation](num1, num2)
+        print(f"Result: {result}")
     else:
-        print("Invalid operation .please choose a valid operation(+ , - , * , /).")    
+        print("Invalid operation. Please choose a valid operation (+, -, *, /).")
 
-#Run the calculator
-Calculator()
+# Run the calculator
+calculator()
